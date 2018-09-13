2ee8463a20ea4ad7a6041145ad5924ef04cc4667
Lemaiyan <george.kamunya@gmail.com>
2018-09-13 14:20:45 +0300

    added the requirements file

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 def get_image_view(request, dimensions, text):
     resp = get_image(dimensions, text)
-    return HttpResponse(resp.content)
+    return HttpResponse(resp.content.encode('utf-8'))
